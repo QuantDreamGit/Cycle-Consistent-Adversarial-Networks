d218d52d73d4097abd0855a89fed2f0fcae9731a
Matteo Tomatis <127085952+Matteo-Tomatis@users.noreply.github.com>
2025-02-02 17:06:43 +0100

    Update eval.py for parallel evaluation

--- eval.py
+++ eval.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import random
 import pickle
 
 import os
@@ -56,12 +57,14 @@ class Evaluator():
         device = self.starGAN.device
         truncation, padding = 'longest_first', 'max_length'
         
-        # Check whether to use pre-trained classifier or discriminator
+        # Verifica se usare il classificatore pre-addestrato o il discriminatore
         if 'lambdas' not in vars(self.args) or self.args.lambdas[4] == 0:
+            # Classificatore pre-addestrato
             classifier = AutoModelForSequenceClassification.from_pretrained(self.args.pretrained_classifier_eval)
             classifier_tokenizer = AutoTokenizer.from_pretrained(self.args.pretrained_classifier_eval)
             classifier.to(device)
         else:
+            # Usa il discriminatore
             classifier = self.starGAN.D  # DiscriminatorModel
             classifier_tokenizer = classifier.tokenizer
         
@@ -69,11 +72,11 @@ class Evaluator():
 
         y_pred, y_true = [], true_domains
 
-        # Batch Processing
+        # Elaborazione batch
         for i in range(0, len(pred_sentences), self.args.batch_size):
             batch_sentences = pred_sentences[i:i + self.args.batch_size]
             
-            # Batch Sentence Tokenization
+            # Tokenizzazione delle frasi in batch
             inputs = classifier_tokenizer(
                 batch_sentences,
                 truncation=truncation,
@@ -85,22 +88,24 @@ class Evaluator():
             with torch.no_grad():
                 # Forward pass
                 if isinstance(classifier, DiscriminatorModel.DiscriminatorModel):
+                    # Usa il discriminatore
                     outputs = classifier(sentences=batch_sentences, device=device)
                     domain_logits = outputs["domain_logits"]
                 else:
+                    # Usa il classificatore pre-addestrato
                     outputs = classifier(**inputs)
-                    domain_logits = outputs.logits  
+                    domain_logits = outputs.logits  # Logits per la classificazione del dominio
                 
-                # Calculating predictions
+                # Calcolo delle predizioni
                 batch_predictions = torch.argmax(domain_logits, dim=1).cpu().tolist()
                 y_pred.extend(batch_predictions)
         
-        # Convert y_true to a list if it is not already
+        # Convertire y_true in una lista se non lo è già
         if isinstance(y_true, torch.Tensor):
             y_true = y_true.cpu().numpy()
         elif isinstance(y_true, list):
-            y_true = np.array(y_true)  
-        y_pred = np.array(y_pred)  
+            y_true = np.array(y_true)  # Per compatibilità con sklearn
+        y_pred = np.array(y_pred)  # y_pred è già su CPU in formato lista
 
         acc = accuracy_score(y_true, y_pred)
         prec = precision_score(y_true, y_pred, average='macro', zero_division=0)
@@ -207,123 +212,84 @@ class Evaluator():
         print(f'End {phase}...')
 
     
-    #TO DO: this function need to be adapted to StarGAN
-    def run_eval_ref(self, epoch, current_training_step, phase, parallel_dl_evalAB, parallel_dl_evalBA):
+    def run_eval_ref(self, epoch, current_training_step, phase, parallel_dl_eval):
         print(f'Start {phase}...')
-        self.cycleGAN.eval() # set evaluation mode
+        self.starGAN.eval()  # Imposta il modello in modalità valutazione
 
         if self.args.comet_logging:
-            if phase == 'validation': context = self.experiment.validate
-            elif phase == 'test': context = self.experiment.test
-        
-        real_A, real_B = [], []
-        pred_A, pred_B = [], []
-        ref_A, ref_B = [], []
-        scores_AB_bleu_self, scores_BA_bleu_self = [], []
-        scores_AB_bleu_ref, scores_BA_bleu_ref = [], []
-        scores_AB_r1_self, scores_BA_r1_self, scores_AB_r2_self, scores_BA_r2_self, scores_AB_rL_self, scores_BA_rL_self = [], [], [], [], [], []
-        scores_AB_r1_ref, scores_BA_r1_ref, scores_AB_r2_ref, scores_BA_r2_ref, scores_AB_rL_ref, scores_BA_rL_ref = [], [], [], [], [], []
-        scores_AB_bscore, scores_BA_bscore = [], []
+            if phase == 'validation':
+                context = self.experiment.validate
+            elif phase == 'test':
+                context = self.experiment.test
 
-        for batch in parallel_dl_evalAB:
-            parallel_a = list(batch[0])
-            references_b = list(batch[1])
+        real_sentences, pred_sentences, ref_sentences = [], [], []
+        scores_bleu_self, scores_bleu_ref = [], []
+        scores_r1_self, scores_r2_self, scores_rL_self = [], [], []
+        scores_r1_ref, scores_r2_ref, scores_rL_ref = [], [], []
+        scores_bscore = []
+
+        for batch in parallel_dl_eval:
+            original_sentences = list(batch[0])  
+            original_styles = list(batch[1])  
+            references = list(batch[2])  # List(str)
+            
+            # If needed, eliminate uppercase letters
             if self.args.lowercase_ref:
-                references_b = [[ref.lower() for ref in refs] for refs in references_b]
+                references = [[ref.lower() for ref in refs] for refs in references]
+            
+            # Randomly select a target style for each sentence
+            target_styles = [random.choice([s for s in range(len(references[0])) if s != orig]) for orig in original_styles]
+            
+            # Save the corresponding reference phrase 
+            selected_references = [references[i][target_styles[i]] for i in range(len(original_sentences))]
+            
             with torch.no_grad():
-                transferred = self.cycleGAN.transfer(sentences=parallel_a, direction='AB')
-            real_A.extend(parallel_a)
-            pred_B.extend(transferred)
-            ref_B.extend(references_b)
-            parallel_a = [[s] for s in parallel_a]
-            scores_AB_bleu_self.extend(self.__compute_metric__(transferred, parallel_a, 'bleu'))
-            scores_AB_bleu_ref.extend(self.__compute_metric__(transferred, references_b, 'bleu'))
-            scores_rouge_self = np.array(self.__compute_metric__(transferred, parallel_a, 'rouge'))
-            scores_AB_r1_self.extend(scores_rouge_self[:, 0].tolist())
-            scores_AB_r2_self.extend(scores_rouge_self[:, 1].tolist())
-            scores_AB_rL_self.extend(scores_rouge_self[:, 2].tolist())
-            scores_rouge_ref = np.array(self.__compute_metric__(transferred, references_b, 'rouge'))
-            scores_AB_r1_ref.extend(scores_rouge_ref[:, 0].tolist())
-            scores_AB_r2_ref.extend(scores_rouge_ref[:, 1].tolist())
-            scores_AB_rL_ref.extend(scores_rouge_ref[:, 2].tolist())
-            if self.args.bertscore: scores_AB_bscore.extend(self.__compute_metric__(transferred, references_b, 'bertscore'))
-            else: scores_AB_bscore.extend([0])
-        avg_AB_bleu_self, avg_AB_bleu_ref = np.mean(scores_AB_bleu_self), np.mean(scores_AB_bleu_ref)
-        avg_AB_bleu_geom = (avg_AB_bleu_self*avg_AB_bleu_ref)**0.5
-        avg_AB_r1_self, avg_AB_r2_self, avg_AB_rL_self = np.mean(scores_AB_r1_self), np.mean(scores_AB_r2_self), np.mean(scores_AB_rL_self)
-        avg_AB_r1_ref, avg_AB_r2_ref, avg_AB_rL_ref = np.mean(scores_AB_r1_ref), np.mean(scores_AB_r2_ref), np.mean(scores_AB_rL_ref)
-        avg_AB_bscore = np.mean(scores_AB_bscore)
+                transferred = self.starGAN.transfer(sentences=original_sentences, target_styles=target_styles)
+            
+            real_sentences.extend(original_sentences)
+            pred_sentences.extend(transferred)
+            ref_sentences.extend(selected_references)
+            
+            # Compute the metrics
+            real_wrapped = [[s] for s in original_sentences]  
+            scores_bleu_self.extend(self.__compute_metric__(transferred, real_wrapped, 'bleu'))
+            scores_bleu_ref.extend(self.__compute_metric__(transferred, selected_references, 'bleu'))
+            scores_rouge_self = np.array(self.__compute_metric__(transferred, real_wrapped, 'rouge'))
+            scores_r1_self.extend(scores_rouge_self[:, 0].tolist())
+            scores_r2_self.extend(scores_rouge_self[:, 1].tolist())
+            scores_rL_self.extend(scores_rouge_self[:, 2].tolist())
+            scores_rouge_ref = np.array(self.__compute_metric__(transferred, selected_references, 'rouge'))
+            scores_r1_ref.extend(scores_rouge_ref[:, 0].tolist())
+            scores_r2_ref.extend(scores_rouge_ref[:, 1].tolist())
+            scores_rL_ref.extend(scores_rouge_ref[:, 2].tolist())
+            
+            if self.args.bertscore:
+                scores_bscore.extend(self.__compute_metric__(transferred, selected_references, 'bertscore'))
+            else:
+                scores_bscore.extend([0] * len(transferred))
 
-        for batch in parallel_dl_evalBA:
-            parallel_b = list(batch[0])
-            references_a = list(batch[1])
-            if self.args.lowercase_ref:
-                references_a = [[ref.lower() for ref in refs] for refs in references_a]
-            with torch.no_grad():
-                transferred = self.cycleGAN.transfer(sentences=parallel_b, direction='BA')
-            real_B.extend(parallel_b)
-            pred_A.extend(transferred)
-            ref_A.extend(references_a)
-            parallel_b = [[s] for s in parallel_b]
-            scores_BA_bleu_self.extend(self.__compute_metric__(transferred, parallel_b, 'bleu'))
-            scores_BA_bleu_ref.extend(self.__compute_metric__(transferred, references_a, 'bleu'))
-            scores_rouge_self = np.array(self.__compute_metric__(transferred, parallel_b, 'rouge'))
-            scores_BA_r1_self.extend(scores_rouge_self[:, 0].tolist())
-            scores_BA_r2_self.extend(scores_rouge_self[:, 1].tolist())
-            scores_BA_rL_self.extend(scores_rouge_self[:, 2].tolist())
-            scores_rouge_ref = np.array(self.__compute_metric__(transferred, references_a, 'rouge'))
-            scores_BA_r1_ref.extend(scores_rouge_ref[:, 0].tolist())
-            scores_BA_r2_ref.extend(scores_rouge_ref[:, 1].tolist())
-            scores_BA_rL_ref.extend(scores_rouge_ref[:, 2].tolist())
-            if self.args.bertscore: scores_BA_bscore.extend(self.__compute_metric__(transferred, references_a, 'bertscore'))
-            else: scores_BA_bscore.extend([0])
-        avg_BA_bleu_self, avg_BA_bleu_ref = np.mean(scores_BA_bleu_self), np.mean(scores_BA_bleu_ref)
-        avg_BA_bleu_geom = (avg_BA_bleu_self*avg_BA_bleu_ref)**0.5
-        avg_BA_r1_self, avg_BA_r2_self, avg_BA_rL_self = np.mean(scores_BA_r1_self), np.mean(scores_BA_r2_self), np.mean(scores_BA_rL_self)
-        avg_BA_r1_ref, avg_BA_r2_ref, avg_BA_rL_ref = np.mean(scores_BA_r1_ref), np.mean(scores_BA_r2_ref), np.mean(scores_BA_rL_ref)
-        avg_BA_bscore = np.mean(scores_BA_bscore)
-        avg_2dir_bleu_ref = (avg_AB_bleu_ref + avg_BA_bleu_ref) / 2
+        # Compute the average values
+        avg_bleu_self, avg_bleu_ref = np.mean(scores_bleu_self), np.mean(scores_bleu_ref)
+        avg_bleu_geom = (avg_bleu_self * avg_bleu_ref) ** 0.5
+        avg_r1_self, avg_r2_self, avg_rL_self = np.mean(scores_r1_self), np.mean(scores_r2_self), np.mean(scores_rL_self)
+        avg_r1_ref, avg_r2_ref, avg_rL_ref = np.mean(scores_r1_ref), np.mean(scores_r2_ref), np.mean(scores_rL_ref)
+        avg_bscore = np.mean(scores_bscore)
 
-        metrics = {'epoch':epoch, 'step':current_training_step,
-                   'self-BLEU A->B':avg_AB_bleu_self, 'self-BLEU B->A':avg_BA_bleu_self,
-                   'ref-BLEU A->B':avg_AB_bleu_ref, 'ref-BLEU B->A':avg_BA_bleu_ref,
-                   'ref-BLEU avg':avg_2dir_bleu_ref,
-                   'g-BLEU A->B':avg_AB_bleu_geom, 'g-BLEU B->A':avg_BA_bleu_geom,
-                   'self-ROUGE-1 A->B':avg_AB_r1_self, 'self-ROUGE-1 B->A':avg_BA_r1_self,
-                   'self-ROUGE-2 A->B':avg_AB_r2_self, 'self-ROUGE-2 B->A':avg_BA_r2_self,
-                   'self-ROUGE-L A->B':avg_AB_rL_self, 'self-ROUGE-L B->A':avg_BA_rL_self,
-                   'ref-ROUGE-1 A->B':avg_AB_r1_ref, 'ref-ROUGE-1 B->A':avg_BA_r1_ref,
-                   'ref-ROUGE-2 A->B':avg_AB_r2_ref, 'ref-ROUGE-2 B->A':avg_BA_r2_ref,
-                   'ref-ROUGE-L A->B':avg_AB_rL_ref, 'ref-ROUGE-L B->A':avg_BA_rL_ref,
-                   'BERTScore A->B':avg_AB_bscore, 'BERTScore B->A':avg_BA_bscore}
+        metrics = {
+            'epoch': epoch, 'step': current_training_step,
+            'self-BLEU': avg_bleu_self, 'ref-BLEU': avg_bleu_ref,
+            'g-BLEU': avg_bleu_geom,
+            'self-ROUGE-1': avg_r1_self, 'self-ROUGE-2': avg_r2_self, 'self-ROUGE-L': avg_rL_self,
+            'ref-ROUGE-1': avg_r1_ref, 'ref-ROUGE-2': avg_r2_ref, 'ref-ROUGE-L': avg_rL_ref,
+            'BERTScore': avg_bscore
+        }
 
         if phase == 'test':
-            acc, prec, rec, f1 = self.__compute_classif_metrics__(pred_A, pred_B)
-            metrics['style accuracy'] = acc
-            metrics['style precision'] = prec
-            metrics['style recall'] = rec
-            metrics['style F1 score'] = f1
-        
-        if phase == 'validation':
-            base_path = f"{self.args.save_base_folder}epoch_{epoch}/"
-            if self.args.eval_strategy == 'epochs':
-                suffix = f'epoch{epoch}'
-                if epoch < self.args.additional_eval:
-                    suffix += f'_step{current_training_step}'
-            else: suffix = f'step{current_training_step}'
-        else:
-            if self.args.from_pretrained is not None:
-                if self.args.save_base_folder is not None:
-                    base_path = f"{self.args.save_base_folder}"
-                else:
-                    base_path = f"{self.args.from_pretrained}epoch_{epoch}/"
-            else:
-                base_path = f"{self.args.save_base_folder}test/epoch_{epoch}/"
-            suffix = f'epoch{epoch}_test'
-            if self.args.from_pretrained and 'GYAFCfm' in self.args.from_pretrained:
-                if 'family' in self.args.path_paral_test_ref: ds = 'family'
-                elif 'music' in self.args.path_paral_test_ref: ds = 'music'
-                suffix += f'_{ds}'
+            acc, prec, rec, f1 = self.__compute_classif_metrics__(pred_sentences)
+            metrics.update({'style accuracy': acc, 'style precision': prec, 'style recall': rec, 'style F1 score': f1})
+
+        base_path = f"{self.args.save_base_folder}{'test/' if phase == 'test' else f'epoch_{epoch}/'}"
+        suffix = f"epoch{epoch}{'_test' if phase == 'test' else ''}"
         os.makedirs(os.path.dirname(base_path), exist_ok=True)
         pickle.dump(metrics, open(f"{base_path}metrics_{suffix}.pickle", 'wb'))
 
@@ -331,32 +297,21 @@ class Evaluator():
             if m not in ['epoch', 'step']:
                 print(f'{m}: {v}')
 
-        df_AB = pd.DataFrame()
-        df_AB['A (source)'] = real_A
-        df_AB['B (generated)'] = pred_B
-        ref_B = np.array(ref_B)
-        for i in range(self.args.n_references):
-            df_AB[f'ref {i+1}'] = ref_B[:, i]
-        df_AB.to_csv(f"{base_path}AB_{suffix}.csv", sep=',', header=True)
-        df_BA = pd.DataFrame()
-        df_BA['B (source)'] = real_B
-        df_BA['A (generated)'] = pred_A
-        ref_A = np.array(ref_A)
-        for i in range(self.args.n_references):
-            df_BA[f'ref {i+1}'] = ref_A[:, i]
-        df_BA.to_csv(f"{base_path}BA_{suffix}.csv", sep=',', header=True)
-        
-        if self.args.comet_logging:    
+        df = pd.DataFrame({'Original': real_sentences, 'Generated': pred_sentences, 'Reference': ref_sentences})
+        df.to_csv(f"{base_path}translations_{suffix}.csv", sep=',', header=True)
+
+        if self.args.comet_logging:
             with context():
-                self.experiment.log_table(f'./AB_{suffix}.csv', tabular_data=df_AB, headers=True)
-                self.experiment.log_table(f'./BA_{suffix}.csv', tabular_data=df_BA, headers=True)
+                self.experiment.log_table(f'./translations_{suffix}.csv', tabular_data=df, headers=True)
                 for m, v in metrics.items():
                     if m not in ['epoch', 'step']:
                         self.experiment.log_metric(m, v, step=current_training_step, epoch=epoch)
-        del df_AB, df_BA
-        print(f'End {phase}...')
-        
 
+        del df
+        print(f'End {phase}...')
+
+        
+    #TO DO: Why don't they work?
     def dummy_classif(self):
         pred_A = ['wake up or you are going to lose your business .',
                   'this place has none of them .',
